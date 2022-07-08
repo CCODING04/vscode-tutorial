357e13fc871def63615c2f81be070b6f07cef839
ccoding <ccoding04@gmail.com>
2022-07-08 15:25:45 +0800

    tips and tricks

--- demo.py
+++ demo.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
+import random
 
 
 def main():
-    print("Hello VSCode!")
+    if random.randint(0, 10) >= 5:
+        print("Hello VSCode!")
+    else:
+        print("Sad you're not")
 
 
 if __name__ == "__main__":
